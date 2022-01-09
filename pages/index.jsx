611aa76614248e39ec7b0de1cb1dece7284cf221
simonjohansson02 <simonjohanssonstoby@gmail.com>
2022-01-09 23:14:32 +0100

    added a header with some meta code and one image

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,13 +1,21 @@
 import Header from '../components/Header/Header';
 import LandingPage from '../components/LandingPage/LandingPage';
 import Projects from '../components/Projects/Projects';
+import Head from 'next/head';
 
 export default function Home() {
 	return (
-		<div className=" w-screen h-screen bg-backgroundColor overflow-x-hidden">
-			<Header />
-			<LandingPage />
-			<Projects />
-		</div>
+		<>
+			<Head>
+				<title>Simon Johanssons | Portfolio</title>
+				<meta name="viewport" content="initial-scale=1.0, width=device-width" />
+				<link rel="icon" href="coding.png" />
+			</Head>
+			<div className=" w-screen h-screen bg-backgroundColor overflow-x-hidden">
+				<Header />
+				<LandingPage />
+				<Projects />
+			</div>
+		</>
 	);
 }
