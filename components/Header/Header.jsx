c604154efbf406a1e66ba16fab9d10a0ee6c24c4
simonjohansson02 <simonjohanssonstoby@gmail.com>
2022-01-11 14:31:06 +0100

    changed cv file and changed position on the header

--- components/Header/Header.jsx
+++ components/Header/Header.jsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 
 const Header = () => {
 	return (
-		<div className="flex flex-row justify-between p-2 pb-12 sm:pr-10 md:pr-20 sm:pl-6 md:pl-10 lg:pl-16  xl:pl-20">
+		<div className="flex flex-row justify-between p-2 pt-10 pb-12 sm:pr-10 md:pr-20 sm:pl-10 md:pl-20 lg:pl-28  xl:pl-40">
 			<div>
 				<h2 className="text-mainColor text-base  sm:text-lg">Hi, my name is</h2>
 				<h1 className="text-secondaryColor text-2xl  sm:text-5xl ">Simon Johansson</h1>
@@ -22,3 +22,4 @@ const Header = () => {
 };
 
 export default Header;
+//sm:pl-6 md:pl-10 lg:pl-16  xl:pl-20
